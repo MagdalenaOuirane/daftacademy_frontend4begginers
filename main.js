fe3b0c7bf60086f425f46d54f16fc31c1847e96b
MagdalenaOuirane <magdalenaouirane@gmail.com>
2019-12-09 22:23:07 +0100

    add event listeners and function

---
+++ main.js
@@ -0,0 +1,77 @@
+//FIRST TASK
+
+
+const hamburger = document.querySelector('.burger');
+
+const iconBurger = document.querySelector('.fa-bars');
+const iconSecondBurger = document.querySelector('.fa-times');
+const asideColumn = document.querySelector('aside');
+
+
+
+hamburger.addEventListener("click", function () {
+    iconBurger.classList.toggle('show');
+    iconSecondBurger.classList.toggle('show');
+    asideColumn.classList.toggle('active');
+})
+
+
+
+
+
+// SECOND TASK
+
+
+const btn = document.querySelector('.button');
+
+btn.addEventListener('click', () => {
+    btn.classList.remove('button');
+    btn.textContent = "Have fun!";
+
+});
+
+
+
+
+//THIRD TASK
+
+//Creating Form Data Object
+
+let FormData = new FormData(formElem);
+
+formData.append("email", "magda@gmail.com");
+formData.append("topic", "concerts");
+
+let blob = new Blob([content], { type: "text/xml" });
+
+let request = new XMLHttpRequest();
+request.open('POST', "");
+request.send(formData);
+
+
+
+
+
+
+const formElem = document.querySelector('contact__form');
+
+formElem.addEventListener('submit', (e) => {
+    e.preventDefault();
+    // to get form values use FormData and Object.fromEntries
+    new FormData(formElem);
+
+    const entries = new Map([
+        ['email', 'magda@gmail.com'],
+        ['topic', 'concerts']
+    ]);
+
+    const obj = Object.fromEntries(entries);
+    console.log(obj);
+
+
+});
+
+
+
+
+
